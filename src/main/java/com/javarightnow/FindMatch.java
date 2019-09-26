3c91ab0668c9f1ea2f8afe99e4baf312a81832dc
Hadi Mohammadi <Hadi.Mohammadi@t-systems.com>
2019-09-26 18:17:47 +0200

    first commit

---
+++ src/main/java/com/javarightnow/FindMatch.java
@@ -0,0 +1,10 @@
+package com.javarightnow;
+
+/**
+ * @author hadi on 9/26/2019.
+ */
+public class FindMatch {
+    public static void main(String[] args) {
+        System.out.println("yay!");
+    }
+}
